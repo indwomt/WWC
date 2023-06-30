826f856805a78272e69694d009403fb1315f8706
Lauren Bentley <laurendebentley@gmail.com>
2023-06-30 13:52:16 -0400

    navbar refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,27 @@
-import React, {useState} from 'react'
-import Nav from 'react-bootstrap/Nav'
-import { NavbarBrand } from 'react-bootstrap'
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+ 
 function Navigation() {
-    return (
-        <>
+  
 
-        
-
-        <Nav className="navigation justify-content-between" activeKey="/home">
-          <NavbarBrand>
-          <h1>WWC</h1>
-        </NavbarBrand>
-        <div className='navLinks d-flex'>
-        <Nav.Item>
-          <Nav.Link href="/home">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Services</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-3">About</Nav.Link>
-        </Nav.Item>
-        </div>
-      </Nav>
-      
-        
-        </>
-    )
+return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation
+
