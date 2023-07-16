c19d3720923fd3d5b5124dbc1dce4b44589bc6fe
Lauren Bentley <laurendebentley@gmail.com>
2023-07-16 17:20:14 -0400

    server.js express removal

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
 const port = process.env.PORT || 3000; // Use the dynamic port from Heroku or fallback to 3000
 
-app.use(express.static(path.join(__dirname, 'dist'))); // Assuming your Vite build output is in the 'dist' folder
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
